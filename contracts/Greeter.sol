d0393cd2660da638b3d144d4f15621910c6b92bc
Sanjay Ananthanarayan <ananthanarayan.sanjay@gmail.com>
2021-12-22 19:33:42 -0600

    trying to get first blockchain project going

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.8.10;
 
 contract Greeter {
-
+    function greet() external pure returns(string memory) {
+        return "Hello, World!";
+    }
 }
--- test/greeter_test.js
+++ test/greeter_test.js
@@ -3,6 +3,30 @@ const GreeterContract = artifacts.require("./contracts/Greeter.sol");
 contract("Greeter", () => {
     it("has been deployed successfully", async () => {
         const greeter = await GreeterContract.deployed();
-        assert(greeter, "contract was not deployed");
+        assert(greeter, "contract failed to deploy");
+    });
+
+    describe("greet()", () => {
+        it("returns 'Hello, World!' ", async () => {
+            const greeter = await GreeterContract.deployed();
+            const expected = "Hello, world!";
+            const actual = await greeter.greet();
+    
+            assert.equal(actual, expected, "greeted with 'Hello, world!'");
+        });
+    });
+});
+
+contract("Greeter: update greeting", () => {
+    describe("setGreeting(string)", () => {
+        it("sets greeting to passed in string", async () => {
+            const greeter = await GreeterContract.deployed();
+            const expected = "Hi there!";
+
+            await greeter.setGreeting(expected);
+            const actual = await greeter.greet();
+    
+            assert.equal(actual, expected, "greeting was not updated");
+        });
     });
 });
