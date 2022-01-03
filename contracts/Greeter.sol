49c7bfa0d55e23662ea99b5705f9f8d7ff3dfe98
Sanjay Ananthanarayan <ananthanarayan.sanjay@gmail.com>
2022-01-03 11:54:09 -0600

    made few changes

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,7 +1,13 @@
 pragma solidity ^0.8.10;
 
 contract Greeter {
+    string private _greeting = "Hello, World!";
+
     function greet() external pure returns(string memory) {
         return "Hello, World!";
+    }
+
+    function setGreeting(string calldata greeting) external {
+        _greeting = greeting;
     }
 }
--- test/greeter_test.js
+++ test/greeter_test.js
@@ -9,10 +9,10 @@ contract("Greeter", () => {
     describe("greet()", () => {
         it("returns 'Hello, World!' ", async () => {
             const greeter = await GreeterContract.deployed();
-            const expected = "Hello, world!";
+            const expected = "Hello, World!";
             const actual = await greeter.greet();
     
-            assert.equal(actual, expected, "greeted with 'Hello, world!'");
+            assert.equal(actual, expected, "greeted with 'Hello, World!'");
         });
     });
 });
